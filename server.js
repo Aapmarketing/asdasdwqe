fa066919715eb53593df8bb2f32164793cc978e0
Aapmarketing <aapmarketing69@gmail.com>
2021-08-06 18:39:09 +0700

    6

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+const omit =require('lodash')
+
+const port = process.env.PORT || 5000;
+const myLiffId = process.env.MY_LIFF_ID;
+
+app.use(express.static('public'));
+
+app.get('/test',function(req,res) {
+    res.json({message:'test'})
+});
+
+app.get('/', function(req, res) {
+    res.json({message:'connected!'});
+    // res.json({id: myLiffId});
+});
+
+app.listen(port, () => console.log(`app listening on port ${port}!`));
+
